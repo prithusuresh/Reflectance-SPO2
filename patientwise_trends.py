cd3c4d45918b50ac8bf85480c0377a2ca03516ea
hticpose <prithvisuresh98@gmail.com>
2020-01-09 19:05:01 +0530

    record error data

--- patientwise_trends.py
+++ patientwise_trends.py
@@ -4,6 +4,7 @@ print (pd.__version__)
 detrended_patientwise = pkl.load(open("detrended.pkl",'rb'))
 
 SPO2_trends = {}
+erroneous_data = []
 SPO2_model = calibrate_and_get_model(False)
 print ("Beginning Run")
 for patient in detrended_patientwise.keys():
@@ -45,13 +46,12 @@ for patient in detrended_patientwise.keys():
             if get_spo2(R_val, SPO2_model) < 80 or get_spo2(R_val, SPO2_model) > 100:
                 meta_dict = {}
                 meta_dict["patient"] = patient
+                meta_dict["SPO2"] = get_spo2(R_val, SPO2_model)
                 meta_dict["Window Index"] = [i,i+wlen]
                 meta_dict["R_values"] = R_val
                 meta_dict["Signal_information"] = return_info(signal, wlen)
                 meta_dict["Components"] = [calculate_R_from_cycle.R_components,calculate_R_from_cycle.IR_components]
-                with open("../erroneous_meta.pkl", "ab") as f:
-                    pkl.dump(meta_dict, f)
-                
+                erroneous_data.append(meta_dict)
 
             print ("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
             
@@ -59,4 +59,7 @@ for patient in detrended_patientwise.keys():
     SPO2_trends[patient] = spo2_runs
     with open("../Patientwise_trend.pkl","wb") as f:
         pkl.dump(SPO2_trends,f)
+with open("../erroneous_meta.pkl", "wb") as f:
+    pkl.dump(erroneous_data, f)
+                
     
