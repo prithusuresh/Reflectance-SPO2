b76f2f3746db9558c81da0a281a974c0bda2794b
hticpose <prithvisuresh98@gmail.com>
2020-01-13 11:50:28 +0530

    Debugged negative SPO2

--- patientwise_trends.py
+++ patientwise_trends.py
@@ -1,62 +1,67 @@
 from utils import return_info,calculate_R_from_cycle,get_spo2, calibrate_and_get_model
 import pickle as pkl
 
+import argparse
+parser = argparse.ArgumentParser(description = "Run SPO2 Calculation for all patients for specifed overlap and window length")
+parser.add_argument('--wlen',help='Specify window length', type = int)
+parser.add_argument('--overlap',help = "Specify overlap % as decimal", type = int)
+args = parser.parse_args()
+
+fs = 600
+wlen = args.wlen*fs
+overlap = args.overlap
+
 detrended_patientwise = pkl.load(open("detrended.pkl",'rb'))
-patientwise_data = pkl.load(open("patientwise_data.pkl","rb"))
+patientwise_data = pkl.load(open("../patientwise_data.pkl","rb"))
 
-TX = ['TX_R',	'TX_IR']
-wrist = ["Probe1_R",	"Probe1_IR"]
-finger = ["Probe2_R",	"Probe2_IR"]
-forehead = ["Probe3_R",	"Probe3_IR"]
-Probe4 = ["Probe4_R",	"Probe4_IR"]
-
+TX = ['TX_R','TX_IR']
+wrist = ["Probe1_R","Probe1_IR"]
+finger = ["Probe2_R","Probe2_IR"]
+forehead = ["Probe3_R","Probe3_IR"]
+Probe4 = ["Probe4_R","Probe4_IR"]
 
 SPO2_trends = {}
 erroneous_data = []
 SPO2_model = calibrate_and_get_model(False)
 print ("Beginning Run")
-for patient in detrended_patientwise.keys():
-    print 
-    fs = 600
-    wlen = 4*fs
 
-    overlap = 0
-
+import pdb;pdb.set_trace()
+for patient in patientwise_data.keys():
+        
     spo2_runs = []
 
-    print ("PATIENT: ",patient, end= " ")
+    print("PATIENT: ",patient, end= " ")
 
-    for i in range(0,len(detrended_patientwise[patient]),int((1-overlap)*wlen) ):
+    for i in range(0,len(patientwise_data[patient]),int((1-overlap)*wlen) ):
         print ("start: ",i, "   end: ",i+wlen)
         
-    raw_signal = patientwise_data[patient].loc[i:i+wlen , TX].values.reshape((-1,2))
+        raw_signal = patientwise_data[patient].loc[i:i+wlen , TX].values.reshape((-1,2))
 
-    R, IR = return_info(raw_signal, wlen, False)
-    if R[0] is None or IR[1] is None:
-        print("Discarding Signal")
-        continue 
+        R, IR = return_info(raw_signal, wlen, False)
+        if R[0] is None or IR[1] is None:
+            print("Discarding Signal")
+            continue 
   # plot_signal(raw_signal,wlen,)
-    R_val = calculate_R_from_cycle(raw_signal,wlen)
-    if R_val is None:
-        print ("SKIPPING")
-        continue
+        R_val = calculate_R_from_cycle(raw_signal,wlen)
+        if R_val is None:
+            print ("SKIPPING")
+            continue
 
-    else:
-        print (R_val)
-        spo2_runs.append(get_spo2(R_val, SPO2_model))
-    
+        else:
+#             print (R_val)
+            spo2_runs.append(get_spo2(R_val, SPO2_model))
 
-        if get_spo2(R_val, SPO2_model) < 80 or get_spo2(R_val, SPO2_model) > 100:
-            meta_dict = {}
-            meta_dict["patient"] = patient
-            meta_dict["SPO2"] = get_spo2(R_val, SPO2_model)
-            meta_dict["Window Index"] = [i,i+wlen]
-            meta_dict["R_values"] = R_val
-            meta_dict["Signal_information"] = return_info(raw_signal, wlen, True)
-            meta_dict["Components"] = [calculate_R_from_cycle.R_components,calculate_R_from_cycle.IR_components]
-            erroneous_data.append(meta_dict)
+            if get_spo2(R_val, SPO2_model) < 80 or get_spo2(R_val, SPO2_model) > 100:
+                meta_dict = {}
+                meta_dict["patient"] = patient
+                meta_dict["SPO2"] = get_spo2(R_val, SPO2_model)
+                meta_dict["Window Index"] = [i,i+wlen]
+                meta_dict["R_values"] = R_val
+                meta_dict["Signal_information"] = return_info(raw_signal, wlen, True)
+                meta_dict["Components"] = [calculate_R_from_cycle.R_components,calculate_R_from_cycle.IR_components]
+                erroneous_data.append(meta_dict)
 
-        print ("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print ("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
         
     
     SPO2_trends[patient] = spo2_runs
--- utils.py
+++ utils.py
@@ -9,6 +9,17 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 plt.rcParams["figure.figsize"] = [30,3]
 
+import os
+import numpy as np 
+import pandas as pd
+import pickle as pkl
+import scipy
+import scipy.signal
+
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
 def detrend(signal):
     R, IR = signal[:,0],signal[:,1]
     R_detrended = fit_trendline(R)[0]
@@ -17,6 +28,9 @@ def detrend(signal):
     signal = np.concatenate([R_detrended,IR_detrended], axis = 1)
 
     return signal
+
+
+
 def fit_trendline(y, fs = 600): 
     '''Fit Trendline for detrending '''
     model = LinearRegression()
@@ -38,6 +52,8 @@ def smooth(signal,window_len=50):
   # detrend, pred, t = fit_trendline(y)
     y = pd.DataFrame(signal).rolling(window_len,center = True, min_periods = 1).mean().values.reshape((-1,))
     return y
+
+
 
 def peaks_and_valleys(signal, prominence = 300, is_smooth = True , distance = 250):
 
@@ -99,22 +115,22 @@ def discard_outliers(signal,peaks,valleys):
     return final_peak,final_val
 
 def return_info(signal,wlen, is_smooth):
-  """ Get smoothened signal, peak location and valley location 
+    """ Get smoothened signal, peak location and valley location 
   Input: 
     signal: RAW SIGNAL"""
 
-  detrended_signal = detrend(signal)
-  R_signal = detrended_signal[:,0].reshape((-1,))
-  IR_signal = detrended_signal[:,1].reshape((-1,))
-  peaks_R, valleys_R = peaks_and_valleys(R_signal, is_smooth)
-  peaks_IR, valleys_IR = peaks_and_valleys(IR_signal, is_smooth)
+    detrended_signal = detrend(signal)
+    R_signal = detrended_signal[:,0].reshape((-1,))
+    IR_signal = detrended_signal[:,1].reshape((-1,))
+    peaks_R, valleys_R = peaks_and_valleys(R_signal, is_smooth)
+    peaks_IR, valleys_IR = peaks_and_valleys(IR_signal, is_smooth)
   
-  if peaks_R is None or valleys_R is None or peaks_IR is None or valleys_IR is None:
-    return [None, None, None], [None, None,None]
-  if is_smooth:
-    return [smooth(signal[:,0]),peaks_R,valleys_R],[smooth(signal[:,1]),peaks_IR,valleys_IR]
-  else:
-    return [signal[:,0], peaks_R,valleys_R],[signal[:,1],peaks_IR,valleys_IR]
+    if peaks_R is None or valleys_R is None or peaks_IR is None or valleys_IR is None:
+        return [None, None, None], [None, None,None]
+    if is_smooth:
+        return [smooth(signal[:,0]),peaks_R,valleys_R],[smooth(signal[:,1]),peaks_IR,valleys_IR]
+    else:
+        return [signal[:,0], peaks_R,valleys_R],[signal[:,1],peaks_IR,valleys_IR]
 
 def plot_signal(signal,wlen, is_smooth = True):
 
@@ -153,6 +169,8 @@ def extract_cycles(data, show = True,harshness = 0.5):
     """extract location of only good cycles """
 
     signal,peaks,valleys = data[0],data[1],data[2]
+    
+    peaks.sort()
   
     val = [[valleys[x-1],valleys[x]] for x in range(1,len(valleys))]
     peak = []
@@ -181,54 +199,55 @@ def extract_cycles(data, show = True,harshness = 0.5):
         return None,None
     
     good_peaks = []
-    i=0
     for i in good_valleys: 
-       good_peaks.append(peak[int(np.where(np.logical_and(peak > i[0], peak < i[1]))[0][0])])
-
+        good_peaks.append(peak[int(np.where(np.logical_and(peak > i[0], peak < i[1]))[0][0])])
+    
+    good_peaks.sort()
     good_valleys = np.asarray(good_valleys)
     return good_peaks, good_valleys
 
 
 def valley_matching(R_valleys, IR_valleys):
-  R_valleys = np.array(R_valleys, ndmin = 2)
-  IR_valleys = np.array(IR_valleys, ndmin = 2)
+    R_valleys = np.array(R_valleys, ndmin = 2)
+    IR_valleys = np.array(IR_valleys, ndmin = 2)
 
-  minimum = min(R_valleys.shape[0], IR_valleys.shape[0])
-  print (minimum)
-  dist = {0:[]}
-  for r in R_valleys:
-    for i in IR_valleys: 
-      if np.sum(abs(i-r)) <= 10:
-        loc = i
+    minimum = min(R_valleys.shape[0], IR_valleys.shape[0])
+#   /rint (minimum)
+    dist = []
+    for r in R_valleys:
+        for i in IR_valleys: 
+            if np.sum(abs(i-r)) <= 10:
+                loc = i
 
-        if not any(loc is x for x in dist[0]):
-          
-          dist[0].append(loc)
-  valleys = dist[0][:minimum]
-  print (valleys)
+                if not any(loc is x for x in dist):
+                    dist.append(loc)
+                    
+    valleys = dist
+#   print (valleys)
   # /[dist[x][0] for x in np.sort(np.array(list(dist.keys())))[:minimum]]
 
-  return valleys
+    return valleys
 
 
 def peak_matching(R_peaks, IR_peaks):
-  R_peaks = np.array(R_peaks)
-  IR_peaks = np.array(IR_peaks)
+    R_peaks = np.array(R_peaks)
+    IR_peaks = np.array(IR_peaks)
   
-  minimum = min(R_peaks.shape[0], IR_peaks.shape[0])
-  print (minimum)
-  dist = {0:[]}
-  for r in R_peaks:
-    for i in IR_peaks: 
-      if np.sum(abs(i-r)) <= 10:
-        loc = i
-        if loc not in dist[0]:
-          dist[0].append(loc)
-  peaks = dist[0][:minimum]
+    minimum = min(R_peaks.shape[0], IR_peaks.shape[0])
+#   print (minimum)
+    dist = []
+    for r in R_peaks:
+        for i in IR_peaks: 
+            if np.sum(abs(i-r)) <= 10:
+                loc = i
+                if loc not in dist:
+                    dist.append(loc)
+#     peaks = dist[0][:minimum]
+    
   # [dist[x][0] for x in np.sort(np.array(list(dist.keys())))[:minimum]]
-  peaks.sort()
-
-  return peaks
+    dist.sort()
+    peaks = dist
+    return peaks
 
 
 
@@ -240,17 +259,21 @@ def calculate_R_from_cycle(signal, wlen, show = False):
     
 
     R,IR = return_info(signal,wlen, True)
-    corr = assess_quality(R,IR)
+
+    if R[1] is None or R[2] is None or IR[1] is None or IR[2] is None:
+        print ("Not Returning R_value. Poor Signal")
+        return None
     peaks_R,valley_groups_R = extract_cycles(R)
     peaks_IR,valley_groups_IR = extract_cycles(IR)
 
-    if peaks_R is None or valley_groups_R is None or peaks_IR is None or valley_groups_IR is None:
+    if peaks_R is None or valley_groups_R is None or peaks_IR is None or valley_groups_IR is None or R[0] is None or IR[0] is None:
     
         print ("Not Returning R_value. Poor Signal")
         return None
     else:
-        print (valley_groups_R, valley_groups_IR)
  
+        
+        corr = assess_quality(R,IR)
  
         if corr > 0.95:
             print ("Good")
@@ -263,6 +286,8 @@ def calculate_R_from_cycle(signal, wlen, show = False):
 
         final_valleys = np.array(valley_matching(valley_groups_R, valley_groups_IR))
         final_peaks = list(set(peak_matching(peaks_R,peaks_IR)))
+        final_peaks.sort()
+
         if show:
             unravel_val = list(set(final_valleys.ravel()))
             unravel_val.sort()
@@ -280,20 +305,16 @@ def calculate_R_from_cycle(signal, wlen, show = False):
 
         R_ratio = ac_dc(R[0], final_peaks, final_valleys)
         calculate_R_from_cycle.R_components = [ac_dc.ac, ac_dc.dc]
+        print ("R_AC AND R_DC :", calculate_R_from_cycle.R_components)
+        
         IR_ratio = ac_dc(IR[0], final_peaks, final_valleys)
         calculate_R_from_cycle.IR_components = [ac_dc.ac, ac_dc.dc]
+        print ("IR_AC AND IR_DC : ", calculate_R_from_cycle.IR_components)
         
         
         if R_ratio is None or IR_ratio is None:
             return None
         else:
-            try:
-                assert R_ratio.shape == IR_ratio.shape
-            except AssertionError:
-                minimum= min(R_ratio.shape[0], IR_ratio.shape[0])
-                R_ratio = R_ratio[:minimum]
-                IR_ratio = IR_ratio[:minimum]                      
-
             R_value = R_ratio/IR_ratio
             return R_value
 
@@ -303,12 +324,29 @@ def ac_dc(signal,peaks, val):
     ac_dc.dc = []
 
     ac_dc.ac = []
+    
+    peaks.sort()
 
-    for i in range(val.shape[0]):
+    val = list(val)
+    i=0
+    while i <len(val):
         try:
-            assert signal[peaks[i]] > (signal[val[i][0]]+signal[val[i][1]])/2
+
+            index = int(np.where(np.logical_and(peaks > val[i][0], peaks < val[i][1]))[0])
+            i+=1
+        except TypeError:
+            val.pop(i)
+
+            
+    for i in range(len(val)):
+        try:
+            assert signal[peaks[i]] > (signal[val[i][0]]+signal[val[i][1]])/2 #and peaks[i]>val[i][0] and peaks[i]<val[i][1]
+            
         except AssertionError:
-            np.delete(val,i)
+            peaks.pop(i)       
+            val.pop(i)
+           
+            i -= 1
             continue
         except IndexError:
             break
@@ -316,14 +354,20 @@ def ac_dc(signal,peaks, val):
         ac_dc.dc.append((signal[val[i][0]]+signal[val[i][1]])/2)
         ac_dc.ac.append(signal[peaks[i]] - (signal[val[i][0]]+signal[val[i][1]])/2)
  
-    print ("SHAPES MATCHING? :", len(ac_dc.dc),len(ac_dc.ac))
+    
     if len(ac_dc.ac) == 0 or len(ac_dc.dc) == 0:
         return None
-
-    ratio = np.array(ac_dc.ac)/np.array(ac_dc.dc)
-
-  
+    
+    try:
+        print ("RATIO: ")
+        ratio = np.array(ac_dc.ac)/np.array(ac_dc.dc)
+        print (ratio)
+    except Exception as e:
+        print (e)
+    
+    
     return ratio
+
 
 def calibrate_and_get_model(show = False):
     with open("Calibration_curve.txt", "r") as f:
@@ -333,6 +377,7 @@ def calibrate_and_get_model(show = False):
     R_truth = np.array([float(x.split("\t")[1].rstrip("\n")) for x in points]).reshape((-1,1))
     SPO2_model = LinearRegression()
     SPO2_model.fit(np.concatenate([R_truth, R_truth**2],axis = 1), spo2)
+
     if show: 
         pred = SPO2_model.predict(np.concatenate([R_truth, R_truth**2],axis = 1))
         plt.plot(R_truth, pred)
